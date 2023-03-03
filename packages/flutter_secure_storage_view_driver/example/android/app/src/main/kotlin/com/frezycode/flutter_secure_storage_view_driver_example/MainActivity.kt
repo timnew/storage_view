c0f4031b8bb1f92848af0bec466b340dd00d4bd9
timnew <1287238+timnew@users.noreply.github.com>
2023-03-03 13:24:23 +1100

    Remove examples that causes version conflicts

--- packages/flutter_secure_storage_view_driver/example/android/app/src/main/kotlin/com/frezycode/flutter_secure_storage_view_driver_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.frezycode.flutter_secure_storage_view_driver_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- packages/shared_preferences_storage_view_driver/example/android/app/src/main/kotlin/com/frezycode/shared_preferences_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.frezycode.shared_preferences_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
